5b4564dce70d5d6b7c5ab3b87c7ee947f4087748
Kevin Krato <kratok08@gmail.com>
2019-11-13 10:22:10 -0700

    Import and use profile related components

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import "./UserProfile.scss"
+import ProfilePicture from '../../components/ProfilePicture/ProfilePicture';
+import ProfileStats from '../../components/ProfileStats/ProfileStats';
+import ProfileBio from '../../components/ProfileBio/ProfileBio';
+import ProfileFeed from '../../components/ProfileFeed/ProfileFeed';
+import ProfileCompleted from '../../components/ProfileCompleted/ProfileCompleted';
 
 export default function UserProfile() {
   return (
@@ -10,6 +15,11 @@ export default function UserProfile() {
         <a href="">anchor</a>
         <button>button</button>
       </nav>
+      <ProfilePicture />
+      <ProfileStats />
+      <ProfileBio />
+      <ProfileFeed />
+      <ProfileCompleted />
     </div>
   )
 }
