7e815a45002181a762ea602cf71a0fbdb2a52bad
Kevin Krato <kratok08@gmail.com>
2019-11-13 14:35:02 -0700

    Update Profile stylings

--- src/components/Profile/ProfileBio/ProfileBio.js
+++ src/components/Profile/ProfileBio/ProfileBio.js
@@ -6,16 +6,20 @@ import "./ProfileBio.scss"
 export default function ProfileBio() {
   return (
     <section className="ProfileBio"> 
-      <h2>Kevin Krato</h2>   
-      <div>
-        <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} size="sm" transform="down-2" /> 
-        <h4>Denver, Colorado</h4>
-      </div>
-      <article>
+      <section className="overview">
+        <h2>Kevin Krato</h2>   
+        <div className="location">
+          <FontAwesomeIcon className="icon" icon={faMapMarkerAlt} size="sm" transform="down-2" /> 
+          <h4>Denver, Colorado</h4>
+        </div>
+      </section>
+      <section className="body">
         <p>I am a graduate of the Turing School of Software and Design. I love designing and building applications!</p>
-      </article>
-      <FontAwesomeIcon className="icon" icon={faLink} size="sm" transform="down-2" /> 
-      <a href="www.kevinkrato.com">www.kevinkrato.com</a>
+        <section className="website-link">
+          <FontAwesomeIcon className="icon" icon={faLink} size="sm" transform="down-2" /> 
+          <a href="www.kevinkrato.com">www.kevinkrato.com</a>
+        </section>
+      </section>
     </section>
   )
 }
