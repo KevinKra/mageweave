6c0313e9ff4739789bf99b518fc220559d3c906f
Kevin Krato <kratok08@gmail.com>
2019-11-13 12:11:22 -0700

    Extract navbar content and return it is a seperate component

--- src/pages/UserProfile/UserProfile.js
+++ src/pages/UserProfile/UserProfile.js
@@ -5,23 +5,19 @@ import ProfileStats from '../../components/Profile/ProfileStats/ProfileStats';
 import ProfileBio from '../../components/Profile/ProfileBio/ProfileBio';
 import ProfileFeed from '../../components/Profile/ProfileFeed/ProfileFeed';
 import ProfileCompleted from '../../components/Profile/ProfileCompleted/ProfileCompleted';
+import NavBar from '../../components/NavBar/NavBar';
 
 export default function UserProfile() {
   return (
     <div className="UserProfile">
-      <nav className="top-nav">
-        <h3>Title</h3>
-        <a href="">anchor</a>
-        <a href="">anchor</a>
-        <button>button</button>
-      </nav>
-<section className="profile-container">
-        <ProfilePicture />
-        <ProfileStats />
-        <ProfileBio />
-        <ProfileFeed />
-        <ProfileCompleted />
-</section>
+      <NavBar />
+      <section className="profile-container">
+              <ProfilePicture />
+              <ProfileStats />
+              <ProfileBio />
+              <ProfileFeed />
+              <ProfileCompleted />
+      </section>
     </div>
   )
 }
