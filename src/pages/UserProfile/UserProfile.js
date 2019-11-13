a98146195d3ce258364a73a8623da92233e88bab
Kevin Krato <kratok08@gmail.com>
2019-11-13 10:04:41 -0700

    Create pages subdirectory

---
+++ src/pages/UserProfile/UserProfile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./UserProfile.scss"
+
+export default function UserProfile() {
+  return (
+    <div className="UserProfile">
+      <nav className="top-nav">
+        <h3>Title</h3>
+        <a href="">anchor</a>
+        <a href="">anchor</a>
+        <button>button</button>
+      </nav>
+    </div>
+  )
+}
