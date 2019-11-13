65261e385b0b8bc92d99ae7ebdd560d75643850d
Kevin Krato <kratok08@gmail.com>
2019-11-13 11:51:08 -0700

    Create streak-tracker template and apply basic styles to ProfileStats card

--- src/components/Profile/ProfileStats/ProfileStats.js
+++ src/components/Profile/ProfileStats/ProfileStats.js
@@ -4,7 +4,20 @@ import "./ProfileStats.scss"
 export default function ProfileStats() {
   return (
     <div className="ProfileStats">
-      <p>ProfileStats</p>
+      <section className="streak-tracker">
+        <h2>Streak:</h2>
+        <div>
+          <h4>Current</h4>
+          <p>7 days</p>
+        </div>
+        <div>
+          <h4>Longest</h4>
+          <p>31 days</p>
+        </div>
+      </section>
+      <section className="achievements">
+
+      </section>
     </div>
   )
 }
