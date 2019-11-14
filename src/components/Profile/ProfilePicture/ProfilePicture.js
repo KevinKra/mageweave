77d50ee07bec40905a8b58971e117ee9b24ad6ef
Kevin Krato <kratok08@gmail.com>
2019-11-14 11:15:58 -0700

    Semantic JSX update, convert p tag to header

--- src/components/Profile/ProfilePicture/ProfilePicture.js
+++ src/components/Profile/ProfilePicture/ProfilePicture.js
@@ -6,13 +6,13 @@ import RankMarker from '../../Widgets/RankMarker/RankMarker';
 export default function ProfilePicture() {
   return (
     <div className="ProfilePicture">
-      <PointsMarker points="500,000,000"/>
       <RankMarker rank="1" />
+      <PointsMarker points="500,000,000"/>
       <section className="image-container">
         <img src="https://avatars1.githubusercontent.com/u/43081008?s=460&v=4" alt=""/>
       </section>
       <section className="text-tag">
-        <p>Always Learning.</p>
+        <p className="motto">Always Learning.</p>
       </section>
     </div>
   )
