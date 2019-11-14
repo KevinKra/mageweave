e987c7676596020e6d41a0a4ccfc7762733bfd67
Kevin Krato <kratok08@gmail.com>
2019-11-13 17:07:04 -0700

    Import and use CompleteBadge in ProfileComplete component

--- src/components/Profile/ProfileCompleted/ProfileCompleted.js
+++ src/components/Profile/ProfileCompleted/ProfileCompleted.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./ProfileCompleted.scss"
+import CompleteBadge from '../../CompleteBadge/CompleteBadge';
 
 export default function ProfileCompleted() {
   return (
-    <div className="ProfileCompleted"> 
-      <p>ProfileCompleted</p>
-    </div>
+    <section className="ProfileCompleted"> 
+      <header>
+        <p>Completed Courses</p>
+      </header>
+      <CompleteBadge title="Intro to JavaScript" score="95" difficulty="4" />
+      <CompleteBadge title="Advanced JavaScript" score="75" difficulty="4" />
+      <CompleteBadge title="OO Best Practices" score="90" difficulty="4" />
+      <CompleteBadge title="AWS Practitioner" score="85" difficulty="3" />
+    </section>
   )
 }
