a61c9cb8440cbd082ed06614e0da27a3dbdbb4ff
Kevin Krato <kratok08@gmail.com>
2019-11-13 17:46:48 -0700

    Update navbar styling so it sizes with main content

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,11 +4,13 @@ import "./NavBar.scss"
 export default function NavBar() {
   return (
     <nav className="NavBar">
-      <h2>MageWeave</h2>
-      <div>
-        <a href="">Home</a>
-        <a href="">Challenges</a>
-        <button>Profile</button>
+      <div className="content">
+        <h2>Mageweave</h2>
+        <div>
+          <a href="">Home</a>
+          <a href="">Challenges</a>
+          <button>Profile</button>
+        </div>
       </div>
     </nav>
   )
