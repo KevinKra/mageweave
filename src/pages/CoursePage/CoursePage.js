0143fa6e2d38f32ce4712a39d736cff6336d3b49
Kevin Krato <kratok08@gmail.com>
2019-11-16 13:33:25 -0700

    Create CoursePage component

---
+++ src/pages/CoursePage/CoursePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CoursePage extends Component {
+  render() {
+    return (
+      <div className="CoursePage">
+        <p>CoursePage</p>
+      </div>
+    )
+  }
+}
