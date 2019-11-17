ebbd3f682ea14d2c4de462f40bf37b15be3581b8
Kevin Krato <kratok08@gmail.com>
2019-11-17 11:05:09 -0700

    Prototype course support content

--- src/pages/CoursePage/CoursePage.js
+++ src/pages/CoursePage/CoursePage.js
@@ -13,16 +13,16 @@ export default class CoursePage extends Component {
         <article className="course-page-content">
             <section className="course-overview">
               <div className="course-section">
-                <h2>The Complete Ruby Course</h2>
+                <h2 className="main-title">The Complete Ruby Course</h2>
                 <div className="chapter-container">
-                  <h3>Ruby Fundamentals</h3>
+                  <h3 className="chapter-title">Ruby Fundamentals</h3>
                   <div className="sub-chapter-level">
                     <LessonLink name="Intro to Ruby"/>
                   </div>
                   <div className="sub-chapter-level">
                     <div className="sibling-topics">
-                      <PracticeLink name="Ruby Methods I Practice" />
-                      <LessonLink name="Ruby Methods I"/>
+                      <PracticeLink name="Ruby Methods I Practice" widget="1/5" />
+                      <LessonLink name="Ruby Methods I" widget="2/4"/>
                     </div>
                     <LessonLink name="Ruby Classes I"/>
                   </div>
@@ -49,9 +49,34 @@ export default class CoursePage extends Component {
                 </div>
               </div>
             </section>
-            <section className="support-content">
-              <p>support</p>
-            </section>
+            <div className="sticky-wrapper">
+              <section className="support-content">
+                <section className="support-progress">
+                  <div className="support-overview">
+                    <h2>Chapter Progress:</h2>
+                    <h3>17%</h3>
+                    <h2>Today's XP:</h2>
+                    <h3>175/1500</h3>
+                  </div>
+                </section>
+                <section className="btn-panel">
+                  <button className="btn-template-1">Flashcards</button>
+                  <button className="btn-template-1">Practice</button>
+                  <button className="btn-template-1">Quiz</button>
+                </section>
+              </section>
+              <section className="support-get-help">
+                <button className="btn-template-3">Ask a Question</button>
+              </section>
+              <section className="social-links">
+                <h5>Follow Mageweave</h5>
+                <div>
+                  <button className="btn-template-2">Twitter</button>
+                  <button className="btn-template-2">Facebook</button>
+                  <button className="btn-template-2">Instagram</button>
+                </div>
+              </section>
+            </div>
         </article>
       </section>
     )
