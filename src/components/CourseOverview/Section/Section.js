2fbc2bd087fee95ad816e25e3ccc42262ad1fb6d
Kevin Krato <kratok08@gmail.com>
2019-11-17 15:26:07 -0700

    Remap component to map over lessons, remove delete Link components

--- src/components/CourseOverview/Section/Section.js
+++ src/components/CourseOverview/Section/Section.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import LessonLink from '../LessonLink/LessonLink';
-import PracticeLink from '../PracticeLink/PracticeLink';
-import TestLink from '../TestLink/TestLink';
+import TopicBadge from '../TopicBadge/TopicBadge';
 import "./Section.scss";
 
 export default function Section(props) {
   const output = 
     props.lessons.map((lesson) => {
-      if (lesson.type === "lesson") {
-        return <LessonLink name={lesson.name} progress={lesson.progress}/>;
-      } else if (lesson.type === "practice") {
-        return <PracticeLink name={lesson.name} />;
-      } else if (lesson.type === "test") {
-        return <TestLink name={lesson.name} />;
-      } else {
-        return <p>ERROR</p>
-      }
+      return <TopicBadge lesson={lesson} type={lesson.type}/>
     })
   
     return (
