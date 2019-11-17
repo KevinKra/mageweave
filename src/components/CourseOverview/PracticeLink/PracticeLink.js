0c1ecbd19dac877e9da8e5e1d0f0ace4020d540f
Kevin Krato <kratok08@gmail.com>
2019-11-17 12:29:36 -0700

    Update practice link props path

--- src/components/CourseOverview/PracticeLink/PracticeLink.js
+++ src/components/CourseOverview/PracticeLink/PracticeLink.js
@@ -5,7 +5,7 @@ import LessonMarker from '../../Widgets/LessonMarker/LessonMarker';
 export default function PracticeLink(props) {
   return (
     <div className="PracticeLink">
-      <LessonMarker widget={props.widget} />
+      <LessonMarker icon="practice" />
       <h4>{props.name}</h4>
     </div>
   )
