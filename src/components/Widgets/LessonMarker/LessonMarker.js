5e61664141f2015def2b08aa4015e6430d878615
Kevin Krato <kratok08@gmail.com>
2019-11-17 12:28:04 -0700

    Integrate conditional behaviors into lesson marker

--- src/components/Widgets/LessonMarker/LessonMarker.js
+++ src/components/Widgets/LessonMarker/LessonMarker.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBolt } from '@fortawesome/free-solid-svg-icons'
 import "./LessonMarker.scss"
 
 export default function LessonMarker(props) {
-  return (
-    <div className="LessonMarker">
-      <p>{props.widget}</p>
-    </div>
-  )
+  if (props.icon === "practice") {
+    return (
+      <div className="LessonMarker icon-format">
+        <FontAwesomeIcon className="icon" icon={faBolt} size="md" transform="down" /> 
+      </div>
+    )
+  } else {
+    return (
+      <div className="LessonMarker">
+        <p>{props.widget}</p>
+      </div>
+    )
+  }
 }
