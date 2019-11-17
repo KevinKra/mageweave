0d347a7218c3384608f700446182c2d9f31fba00
Kevin Krato <kratok08@gmail.com>
2019-11-17 09:27:21 -0700

    Integrate LessonMarker with related course components

--- src/components/Course/LessonLink/LessonLink.js
+++ src/components/Course/LessonLink/LessonLink.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./LessonLink.scss"
+import LessonMarker from '../../Widgets/LessonMarker/LessonMarker';
 
 export default function LessonLink(props) {
   return (
     <div className="LessonLink">
+      <LessonMarker widget={props.widget} />
       <h4>{props.name}</h4>
     </div>
   )
--- src/components/Course/PracticeLink/PracticeLink.js
+++ src/components/Course/PracticeLink/PracticeLink.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./PracticeLink.scss"
+import LessonMarker from '../../Widgets/LessonMarker/LessonMarker';
 
 export default function PracticeLink(props) {
   return (
     <div className="PracticeLink">
+      <LessonMarker widget={props.widget} />
       <h4>{props.name}</h4>
     </div>
   )
