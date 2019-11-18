6b99fdd58a34353b3a83986d97f186905e888d74
Kevin Krato <kratok08@gmail.com>
2019-11-18 07:28:14 -0700

    Mock add locked prop

--- src/components/CourseOverview/Overview/Overview.js
+++ src/components/CourseOverview/Overview/Overview.js
@@ -2,13 +2,13 @@ import React from 'react'
 import Chapter from '../Chapter/Chapter';
 import "./Overview.scss"
 
-export default function Overview(props) {
+export default function Overview({data}) {
   return (
     <section className="course-overview">
       <div className="course-section">
-        <h2 className="main-title">{props.data.mainTitle}</h2>
-        <Chapter title={props.data.chapters[0].chapterTitle} sections={props.data.chapters[0].sections}/>
-        <Chapter title={props.data.chapters[1].chapterTitle} sections={props.data.chapters[1].sections}/>
+        <h2 className="main-title">{data.mainTitle}</h2>
+        <Chapter title={data.chapters[0].chapterTitle} sections={data.chapters[0].sections} locked={data.chapters[0].locked}/>
+        <Chapter title={data.chapters[1].chapterTitle} sections={data.chapters[1].sections} locked={data.chapters[1].locked}/>
       </div>
     </section>
   )
