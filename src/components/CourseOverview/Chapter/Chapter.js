6fa53a9631c4675635e06673c778c727ba71ee6a
Kevin Krato <kratok08@gmail.com>
2019-11-18 07:27:30 -0700

    Destructure component

--- src/components/CourseOverview/Chapter/Chapter.js
+++ src/components/CourseOverview/Chapter/Chapter.js
@@ -2,16 +2,16 @@ import React from 'react'
 import Section from '../Section/Section';
 import "./Chapter.scss";
 
-export default function Chapter(props) {
-  const sections = 
-      props.sections.map((lessons, i) => {
-        return <Section key={i} lessons={lessons}/>
+export default function Chapter({sections, locked, title}) {
+  const outputSections = 
+      sections.map((lessons, i) => {
+        return <Section key={i} lessons={lessons} locked={locked}/>
       });
 
   return (
     <section className="Chapter">
-      <h3 className="chapter-title">{props.title}</h3>
-      {sections}
+      <h3 className="chapter-title">{title}</h3>
+      {outputSections}
     </section>
   )
 }
