48670fef0f41258ee7a86065b1fede1c3944f4f4
Kevin Krato <kratok08@gmail.com>
2019-11-18 15:03:40 -0700

    Add content and styles to CourseCard component

--- src/components/CourseCard/CourseCard.js
+++ src/components/CourseCard/CourseCard.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {withRouter} from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faClock } from '@fortawesome/free-solid-svg-icons'
 import "./CourseCard.scss";
 
 class CourseCard extends Component {
@@ -8,9 +10,34 @@ class CourseCard extends Component {
   }
   render() {
     return (
-      <div className="CourseCard" onClick={this.handleRedirect}>
-        <p>{this.props.name}</p>
-      </div>
+      <article className="CourseCard" onClick={this.handleRedirect}>
+        <div className="card-interior">
+          <div className="card-details">
+            <h4>{this.props.name}</h4>
+            <p>by <span>{this.props.author}</span></p>
+          </div>
+          <div className="card-stats">
+            <div>
+              <FontAwesomeIcon 
+                className="icon" 
+                icon={faUser} 
+                size="md" 
+                transform="down" 
+              /> 
+              <p>{this.props.totalUsers}</p>
+            </div>
+            <div>
+              <FontAwesomeIcon 
+                  className="icon" 
+                  icon={faClock} 
+                  size="md" 
+                  transform="down" 
+                /> 
+              <p>{this.props.estimatedDuration}</p>
+            </div>
+          </div>
+        </div>
+      </article>
     )
   }
 }
