c16f0497e8644fba040437a9ad8f15143a8f63e5
Kevin Krato <kratok08@gmail.com>
2019-11-18 15:04:17 -0700

    Add additional mock data and limit width of container in styles

--- src/components/CourseOverview/Overview/Overview.js
+++ src/components/CourseOverview/Overview/Overview.js
@@ -9,6 +9,7 @@ export default function Overview({data}) {
         <h2 className="main-title">{data.mainTitle}</h2>
         <Chapter title={data.chapters[0].chapterTitle} sections={data.chapters[0].sections} locked={data.chapters[0].locked}/>
         <Chapter title={data.chapters[1].chapterTitle} sections={data.chapters[1].sections} locked={data.chapters[1].locked}/>
+        <Chapter title={data.chapters[2].chapterTitle} sections={data.chapters[2].sections} locked={data.chapters[2].locked}/>
       </div>
     </section>
   )
