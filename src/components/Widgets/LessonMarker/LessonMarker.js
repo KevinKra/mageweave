94933bf5433f363419a85386144dbda51f70cd83
Kevin Krato <kratok08@gmail.com>
2019-11-18 07:30:14 -0700

    Add additional challenge icons and associated styles

--- src/components/Widgets/LessonMarker/LessonMarker.js
+++ src/components/Widgets/LessonMarker/LessonMarker.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBolt } from '@fortawesome/free-solid-svg-icons'
+import { faBolt, faPencilRuler, faCode } from '@fortawesome/free-solid-svg-icons'
 import "./LessonMarker.scss"
 
 export default function LessonMarker(props) {
   if (props.icon === "practice") {
     return (
-      <div className="LessonMarker icon-format">
+      <div className="LessonMarker icon-bolt">
         <FontAwesomeIcon className="icon" icon={faBolt} size="md" transform="down" /> 
+      </div>
+    )
+  } else if (props.icon === "project") {
+    return (
+      <div className="LessonMarker icon-project">
+        <FontAwesomeIcon className="icon" icon={faPencilRuler} size="md" transform="down" /> 
+      </div>
+    )
+  } else if (props.icon === "challenge") {
+    return (
+      <div className="LessonMarker icon-challenge">
+        <FontAwesomeIcon className="icon" icon={faCode} size="md" transform="down" /> 
       </div>
     )
   } else {
